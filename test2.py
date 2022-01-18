34e994ba6db6155c6af0ba168b54cff4f40aa8a3
Jim <chengxuyuan9527@gmail.com>
2022-01-18 13:18:16 +1100

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,2 +1,9 @@
-if 5>2:
-    print("Five is greater than two!")
+# if 5>2:
+#     print("Five is greater than two!")
+c = 5
+while c < 10:
+    print(c)
+    c = c + 1
+# This is a while loop
+
+print("Hello World")
