9deec4e2a3626195a6b426732a8fe7fd11720c4b
Jim <chengxuyuan9527@gmail.com>
2022-01-18 13:27:22 +1100

    Update test2.py
    variables

--- test2.py
+++ test2.py
@@ -1,5 +1,8 @@
 # if 5>2:
 #     print("Five is greater than two!")
+from re import M
+
+
 c = 5
 while c < 10:
     print(c)
@@ -7,3 +10,10 @@ while c < 10:
 # This is a while loop
 
 print("Hello World")
+
+my_age = 35
+
+my_name = "Jim Liu"
+
+print(my_age)
+print(my_name)
