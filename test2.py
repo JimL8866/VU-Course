02bfe8317dc22ab6a1d97432a11b18d1e2479b58
Jim <chengxuyuan9527@gmail.com>
2022-01-18 20:53:22 +1100

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,19 +1,35 @@
-# if 5>2:
-#     print("Five is greater than two!")
-from re import M
+# # if 5>2:
+# #     print("Five is greater than two!")
+# from re import M
+
+# c = 5
+# while c < 10:
+#     print(c)
+#     c = c + 1
+# # This is a while loop
+
+# print("Hello World")
+
+# my_age = 35
+# my_name = "Jim Liu"
+
+# print(my_age)
+# print(my_name)
 
 
-c = 5
-while c < 10:
-    print(c)
-    c = c + 1
-# This is a while loop
+# my_lunch_meal = ["apple", "juice", "rice"]
+# print(type(my_lunch_meal))
 
-print("Hello World")
 
-my_age = 35
+# my_variable = 19
+# my_string = str(my_variable)
+# print(my_string)
+# print(type(my_string))
 
-my_name = "Jim Liu"
+# my_string2="a"
+# my_int2 = bytes(my_string2)
+# print(my_string2)
 
-print(my_age)
-print(my_name)
+my_char = "A"
+my_int = ord(my_char)
+print(my_int)
