18ea92085d912d5514097bc21615aee96ca5dea3
Jim <chengxuyuan9527@gmail.com>
2022-01-18 20:53:26 +1100

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,12 @@
+# my_address = "BundooraMelbourne"
+# print(my_address[2:6])
+
+my_adress = "Bundoora.Melbourne.Australia"
+print(my_adress.split("."))
+
+
+my_name ="jim liu"
+my_address = "Bundoora"
+print(my_name + "$" + my_address)
+
+print("I was walking\n\"in\" the park ")
