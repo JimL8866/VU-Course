7c8fdb6381ffb6a5e92888ac5dddbde43bc6d8b2
Jim <chengxuyuan9527@gmail.com>
2022-01-19 10:21:23 +1100

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -2,10 +2,21 @@
 # y = 14
 # print(x+y)
 
-x = 21
-y = 2
-print(x % y)
+# x = 21
+# y = 2
+# print(x % y)
 
-x= 12
-x +=3
-print(x)
+# x= 12
+# x +=3
+# print(x)
+
+my_breakfast=["apple", "milk","bacon","water"]
+print(my_breakfast)
+print(my_breakfast.count("milk"))
+print(my_breakfast[1])
+
+
+print(my_breakfast[0].count("p"))
+my_breakfast[2]="chicken"
+print(my_breakfast)
+
