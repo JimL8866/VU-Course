ea606733ee633d51310744713447a2963838a09b
Jim <chengxuyuan9527@gmail.com>
2022-01-19 11:37:39 +1100

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -21,8 +21,34 @@ my_breakfast=["apple", "milk","bacon","water"]
 # print(my_breakfast)
 
 my_breakfast.append("too much breakfast")
-print(my_breakfast)
-age =int(input("what is your age?"))
-your_brother_age =int(input("what is your brother age?"))
+# print(my_breakfast)
+# age =int(input("what is your age?"))
+# your_brother_age =int(input("what is your brother age?"))
 
-print(age+your_brother_age)
+# print(age+your_brother_age)
+newlist= my_breakfast.copy()
+print(newlist)
+
+# string1= int(input("Guess a number"))
+# string2 = int(input("Guess the second number"))
+
+# if string1 > string2:
+#     print("That is correct, move on")
+# else:
+#     print("Do it again")
+
+
+# age1 = int(input("What is your age?"))
+# age2 = int(input("What is your brother age?"))
+
+# if age1 > age2:
+#     print("You are older")
+
+# else:
+#     print("You are younger. Well done")
+
+int1 = int(input("what is your number"))
+i = 0
+while i <5: 
+    print(int1)
+    i +=1
