c824e66596ad3098fd63a2de391f2fa76dfcfa18
Jim <chengxuyuan9527@gmail.com>
2022-01-19 14:38:26 +1100

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -10,10 +10,10 @@
 # x +=3
 # print(x)
 
-from operator import truediv
+# from operator import truediv
 
 
-my_breakfast=["apple", "milk","bacon","water"]
+# my_breakfast=["apple", "milk","bacon","water"]
 # print(my_breakfast)
 # print(my_breakfast.count("milk"))
 # print(my_breakfast[1])
@@ -23,14 +23,14 @@ my_breakfast=["apple", "milk","bacon","water"]
 # my_breakfast[2]="chicken"
 # print(my_breakfast)
 
-my_breakfast.append("too much breakfast")
+# my_breakfast.append("too much breakfast")
 # print(my_breakfast)
 # age =int(input("what is your age?"))
 # your_brother_age =int(input("what is your brother age?"))
 
 # print(age+your_brother_age)
-newlist= my_breakfast.copy()
-print(newlist)
+# newlist= my_breakfast.copy()
+# print(newlist)
 
 # string1= int(input("Guess a number"))
 # string2 = int(input("Guess the second number"))
@@ -56,7 +56,7 @@ print(newlist)
 #     print(int1+1)
 #     i +=1
 
-string = "myString"
+# string = "myString"
 # for char in string:
 #     print(char)
 
@@ -65,25 +65,41 @@ string = "myString"
 #     if char == "i":
 #         break
 
-my_breakfast=["apple", "milk","bacon","water","Too much"]
-num=int(input("pleaes put a number"))
-if num < len(my_breakfast)-1:
-    print (my_breakfast[num])
-else:
-    print("try again")
+# my_breakfast=["apple", "milk","bacon","water","Too much"]
+# num=int(input("pleaes put a number"))
+# if num < len(my_breakfast)-1:
+#     print (my_breakfast[num])
+# else:
+#     print("try again")
 
-my_letter ="bundoor"
-for item in my_letter:
-    print(item)
-    for i in range(10):
-        print(i)
+# my_letter ="bundoor"
+# for item in my_letter:
+#     print(item)
+#     for i in range(10):
+#         print(i)
 
-i = 0
-while True:
-    i = i + 1
-    print(i)
-    num = input("put a number")
-    if num =="q":
-        break
-    else:
-        continue
+# i = 0
+# while True:
+#     i = i + 1
+#     print(i)
+#     num = input("put a number")
+#     if num =="q":
+#         break
+#     else:
+#         continue
+
+
+# def mutiple(num):
+#     a = 5
+#     b = a* num
+#     print(b)
+
+# mutiple(5)
+
+def mod(num):
+    result = num % 2
+    if result == 0:
+        print("even number")
+
+
+mod(10)
