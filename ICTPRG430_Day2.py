05d0241d03afece4ee042511516d0ef9c64ed7c2
Jim <chengxuyuan9527@gmail.com>
2022-02-09 09:58:02 +1100

    Update ICTPRG430_Day2.py

--- ICTPRG430_Day2.py
+++ ICTPRG430_Day2.py
@@ -14,3 +14,20 @@ car1.display()
 car2.display()
 print(car1.driver)
 print(car2.driver)
+
+################################################################################
+class Driver:
+    demerit = 12
+    def __init__(self):
+        pass
+
+
+
+name = Driver()
+print(Driver.demerit)
+print(name.demerit)
+print(id(Driver.demerit))
+print(id(name.demerit))
+name.demerit = 9
+print(name.demerit, Driver.demerit)
+print(id(name.demerit), id(Driver.demerit))
