d6a89fb018952260c6881858f79549c08ac719c1
Jim <chengxuyuan9527@gmail.com>
2022-02-09 11:09:41 +1100

    Update ICTPRG430_Day2.py

--- ICTPRG430_Day2.py
+++ ICTPRG430_Day2.py
@@ -1,19 +1,19 @@
-class Car:
-    driver = "Tom"
+# class Car:
+#     driver = "Tom"
 
-    def __init__(self, speed):
-        self.speed = speed
+#     def __init__(self, speed):
+#         self.speed = speed
 
-    def display(self):
-        print(f"The car speed is {self.speed}")
+#     def display(self):
+#         print(f"The car speed is {self.speed}")
 
 
-car1 = Car(100)
-car2 = Car(120)
-car1.display()
-car2.display()
-print(car1.driver)
-print(car2.driver)
+# car1 = Car(100)
+# car2 = Car(120)
+# car1.display()
+# car2.display()
+# print(car1.driver)
+# print(car2.driver)
 
 ################################################################################
 class Driver:
@@ -31,3 +31,21 @@ print(id(name.demerit))
 name.demerit = 9
 print(name.demerit, Driver.demerit)
 print(id(name.demerit), id(Driver.demerit))
+
+
+#########################################################
+class Computer:
+    def __init__(self, name, value):
+        self.name = name
+        self.__value = value
+
+    def get_value(self):
+        print(self.__value)
+
+
+my_computer = Computer("Lenovo", 1000)
+# print(my_computer.__value)
+
+print(my_computer._Computer__value)
+
+my_computer.get_value()
