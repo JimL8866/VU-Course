1445e48f7214166e7b17174895ed99414281f927
Jim <chengxuyuan9527@gmail.com>
2022-02-09 14:07:49 +1100

    Update ICTPRG430_Day2.py

--- ICTPRG430_Day2.py
+++ ICTPRG430_Day2.py
@@ -16,53 +16,81 @@
 # print(car2.driver)
 
 ################################################################################
-class Driver:
-    demerit = 12
-    def __init__(self):
-        pass
+# class Driver:
+#     demerit = 12
+#     def __init__(self):
+#         pass
 
 
 
-name = Driver()
-print(Driver.demerit)
-print(name.demerit)
-print(id(Driver.demerit))
-print(id(name.demerit))
-name.demerit = 9
-print(name.demerit, Driver.demerit)
-print(id(name.demerit), id(Driver.demerit))
+# name = Driver()
+# print(Driver.demerit)
+# print(name.demerit)
+# print(id(Driver.demerit))
+# print(id(name.demerit))
+# name.demerit = 9
+# print(name.demerit, Driver.demerit)
+# print(id(name.demerit), id(Driver.demerit))
 
 
 #########################################################
-class Computer:
-    def __init__(self, name, value):
-        self.name = name
-        self.__value = value
+# class Computer:
+#     def __init__(self, name, value):
+#         self.name = name
+#         self.__value = value
 
-    def get_value(self):
-        print(self.__value)
+#     def get_value(self):
+#         print(self.__value)
 
 
-my_computer = Computer("Lenovo", 1000)
-# print(my_computer.__value)
+# my_computer = Computer("Lenovo", 1000)
+# # print(my_computer.__value)
 
-print(my_computer._Computer__value)
+# print(my_computer._Computer__value)
 
-my_computer.get_value()
+# my_computer.get_value()
 
 #################################################
-class Phone:
-    def __init__(self, name, brand):
+# class Phone:
+#     def __init__(self, name, brand):
+#         self.name = name
+#         self.__brand = None
+
+#     def set_value(self):
+#         self.__brand = "iphone"
+
+#     def get_value(self):
+#         return self.__brand
+
+
+# my_phone = Phone("Jim's phone", "iphone")
+# my_phone.set_value()
+# print(my_phone.get_value())
+
+
+##################################################
+
+class Appliance:
+    def __init__(self, name, colour):
         self.name = name
-        self.__brand = None
+        self.colour = colour
 
-    def set_value(self):
-        self.__brand = "iphone"
-
-    def get_value(self):
-        return self.__brand
+    def func(self):
+        print(f"{self.name} can cooking meals")
 
 
-my_phone = Phone("Jim's phone", "iphone")
-my_phone.set_value()
-print(my_phone.get_value())
+class Microwave(Appliance):
+    def __init__(self, value, weight):
+        super().__init__("LG", "1000")
+        self.value = value
+        self.weight = weight
+
+    def func(self):
+        super().func()
+        print(f"{self.name} can make meals hot")
+
+
+my_fridge = Microwave("500", "100KG")
+my_fridge.func()
+print(my_fridge.name)
+print(my_fridge.colour)
