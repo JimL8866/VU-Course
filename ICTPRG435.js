eb719ff691517d3a4c8ade0d8e9e9de5a662c955
Jim <chengxuyuan9527@gmail.com>
2022-03-23 12:32:13 +1100

    practice in class

---
+++ ICTPRG435.js
@@ -0,0 +1,10 @@
+var m = prompt("Enter value of m")
+console.log(`initial value of m is ${m}`)
+var n = prompt("Enter value of n")
+console.log(`initial value of n is ${n}`)
+var temp =""
+temp = m
+m = n
+n = temp
+console.log(`The value of m is ${m}`)
+console.log(`The value of n is ${n}`)
